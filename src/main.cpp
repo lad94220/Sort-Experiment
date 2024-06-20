85535725f43c4a187e4aea871d4ccf06b032381e
lad94220 <laduy231@clc.fitus.edu.vn>
2024-06-20 15:25:34 +0700

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,19 @@ int main(int argc, char** argv) {
     
     if (strcmp(argv[1], "-a") == 0) {
         if (argc == 5) {
-            if (isNum(argv[3]) != 0) {
-
+            if (isNum(argv[3]) == 0) {
+                string agro = argv[2];
+                if (agro == "selection-sort");
+                if (agro == "insertion-sort");
+                if (agro == "shell-sort");
+                if (agro == "bubble-sort");
+                if (agro == "heap-sort");
+                if (agro == "merge-sort");
+                if (agro == "quick-sort");
+                if (agro == "radix-sort");
+                if (agro == "counting-sort");
+                if (agro == "shaker-sort");
+                if (agro == "flash-sort");
             } else {
 
             }
