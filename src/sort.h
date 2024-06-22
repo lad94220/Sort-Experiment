3c223ab61986764c6d2c423911a7b9da5353568a
lad94220 <laduy231@clc.fitus.edu.vn>
2024-06-22 15:58:38 +0700

    initial commit

--- src/sort.h
+++ src/sort.h
@@ -3,8 +3,16 @@ using namespace std;
 
 //runtime
 void selectionSort(int* src, int n);
-
-
+void insertionSort(int* src, int n);
+void bubbleSort(int* src, int n);
+void heapSort(int* src, int n);
+void mergeSort(int* src, int n);
+void quickSort(int* src, int n);
+void radixSort(int* src, int n);
+void shakerSort(int* src, int n);
+void shellSort(int* src, int n);
+void countingSort(int* src, int n);
+void flashSort(int* src, int n);
 
 //count experiments
 void selectionSort_count(int* src, int n, int& count_compare);
