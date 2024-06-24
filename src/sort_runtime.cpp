698bdebc6a78cd53145a76a942faf63fc9cf4de6
Nhật Minh Trương <trnhmink@gmail.com>
2024-06-24 11:36:21 +0700

    initial commit

--- src/sort_runtime.cpp
+++ src/sort_runtime.cpp
@@ -7,3 +7,50 @@ void selectionSort(int* src, int n) {
         }
     }
 }
+
+void minHeapRebuild(int* src, int n, int pos) {
+	int _pos = pos;
+
+	while (2 * _pos + 1 < n) {
+		int child = 2 * _pos + 1;
+
+		if (child < n - 1)
+			if (src[child] > src[child + 1]) child += 1;
+		if (src[_pos] > src[child]) {
+			swap(src[_pos], src[child]);
+			_pos = child;
+		}
+		else break;
+	}
+}
+void minHeapBuild(int* src, int n) {
+	for (int i = (n - 2) / 2; i >= 0; i--)
+		minHeapRebuild(src, n, i);
+}
+void heapSort(int* src, int n) {
+	int m = n;
+	for (int* tmp = src; tmp < src + n; tmp++)
+		minHeapBuild(tmp, m--);
+}
+
+void digitSort(int* src, int n, int exp) {
+	int* pos = new int[10]();
+	int* dst = new int[n];
+
+	for (int i = 0; i < n; i++)
+		pos[(src[i] / exp) % 10]++;
+	for (int i = 1; i < 10; i++)
+		pos[i] += pos[i - 1];
+	for (int i = n - 1; i >= 0; i--)
+		dst[--pos[(src[i] / exp) % 10]] = src[i];
+	for (int i = 0; i < n; i++)
+		src[i] = dst[i];
+
+	delete[]dst;
+	delete[]pos;
+}
+void radixSort(int* src, int n) {
+	int mx = *max_element(src, src + n);
+	for (int exp = 1; mx / exp > 0; exp *= 10)
+		digitSort(src, n, exp);
+}
